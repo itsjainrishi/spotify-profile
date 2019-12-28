e3217dff56c375854cde642909244d4b72fbf84c
Rishi Jain <rishi.jaipur17@gmail.com>
2019-12-28 17:44:00 +0530

    Setting up API

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const redis = require('async-redis');
+const axios = require('axios');
+
 require('dotenv').config();
 
 const app = express()
@@ -16,7 +18,6 @@ function connectToRedis() {
   return redisClient
 }
 
-app.all('/spotify/data/:key', async ({ params: { key }, query }, res) => {
 
 /*
   req.params is an object so instead of using req.params.key 
@@ -24,6 +25,7 @@ app.all('/spotify/data/:key', async ({ params: { key }, query }, res) => {
   Please google nested object destructuring if you are still confused.
 */ 
 
+app.all('/spotify/data/:key', async ({ params: { key }, query }, res) => {
   try {
     if (key === ('refresh_token' || 'access_token'))
       throw { error: 'Cannot get protected stores.' }
@@ -55,6 +57,144 @@ async function callStorage(method, ...args) {
   const response = await redisClient[method](...args)
   redisClient.quit()
   return response
+}
+
+
+app.get('/spotify/callback', async ({ query: { code } }, res) => {
+  try {
+    const { data } = await getSpotifyToken({
+      code,
+      grant_type: 'authorization_code'
+    })
+    const { access_token, refresh_token, expires_in } = data
+    const {
+      data: { id }
+    } = await getUserData(access_token)
+
+    if (id !== process.env.SPOTIFY_USER_ID)
+      throw "You aren't the droid we're looking for."
+
+    callStorage(...storageArgs('is_connected', { value: true }))
+    callStorage(...storageArgs('refresh_token', { value: refresh_token }))
+    callStorage(
+      ...storageArgs('access_token', {
+        value: access_token,
+        expires: expires_in
+      })
+    )
+
+    const success = 'Welcome Back'
+    res.redirect(`/auth?success=${success}`)
+  } catch (err) {
+    console.error(
+      `\n There was an error at /api/spotify/callback: ${err} \n`
+    )
+    res.redirect(`/auth?message=${err}`)
+  }
+})
+
+
+/*
+  single expression so using implicit return. 
+  You can read about implicit returns here :
+  https://jaketrent.com/post/javascript-arrow-function-return-rules/  
+*/
+
+const getSpotifyToken = (props = {}) => 
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    params: {
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+      redirect_uri: `${process.env.CLIENT_URL}/api/spotify/callback`,
+      ...props
+    },
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+
+const spotifyBaseUrl = 'https://api.spotify.com/v1/'
+
+const getUserData = access_token =>
+  axios.get(`${spotifyBaseUrl}me`, {
+    headers: {
+      withCredentials: true,
+      Authorization: `Bearer ${access_token}`
+    }
+  })
+
+async function getAccessToken() {
+  const redisClient = connectToRedis()
+  const accessTokenObj = { value: await redisClient.get('access_token') }
+  if (!Boolean(accessTokenObj.value)) {
+    const refresh_token = await redisClient.get('refresh_token')
+    const {
+      data: { access_token, expires_in }
+    } = await getSpotifyToken({
+      refresh_token,
+      grant_type: 'refresh_token'
+    })
+    Object.assign(accessTokenObj, {
+      value: access_token,
+      expires: expires_in
+    })
+    callStorage(...storageArgs('access_token', { ...accessTokenObj }))
+  }
+  redisClient.quit()
+  return accessTokenObj.value
+}
+
+app.get('/spotify/now-playing/', async (req, res) => {
+  try {
+    const access_token = await getAccessToken()
+    const response = await axios.get(
+      `${spotifyBaseUrl}me/player/currently-playing?market=US`,
+      {
+        headers: {
+          withCredentials: true,
+          Authorization: `Bearer ${access_token}`
+        }
+      }
+    )
+    const { data } = response
+    setLastPlayed(access_token, data)
+    const reply = await callStorage('get', 'last_played')
+    res.send({
+      item: JSON.parse(reply),
+      is_playing: Boolean(data.is_playing),
+      progress_ms: data.progress_ms || 0
+    })
+  } catch (err) {
+    console.error(err)
+    res.send({ error: err.message })
+  }
+})
+
+async function setLastPlayed(access_token, item) {
+  if (!Boolean(item)) {
+    const { data } = await axios.get(
+      `${spotifyBaseUrl}me/player/recently-played?market=US`,
+      {
+        headers: {
+          withCredentials: true,
+          Authorization: `Bearer ${access_token}`
+        }
+      }
+    )
+    postStoredTrack(data.items[0].track)
+  } else {
+    postStoredTrack(item.item)
+  }
+}
+
+function postStoredTrack(props) {
+  callStorage(
+    ...storageArgs('last_played', {
+      body: props
+    })
+  )
 }
 
 module.exports = {
